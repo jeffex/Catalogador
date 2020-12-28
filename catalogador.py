3115064cc0cbd9c30e6146254b669e15f84b9b5b
Jerônimo Luis Ferreira Filho <37778906+jeffex@users.noreply.github.com>
2020-12-28 11:47:36 -0300

    Configurações em arquivo
    Dados de conta e configurações em arquivo de texto, e possibilidade de catalogar mais de um timeframe por execução

--- catalogador.py
+++ catalogador.py
@@ -2,30 +2,42 @@ from iqoptionapi.stable_api import IQ_Option
 from datetime import datetime, timedelta
 from colorama import init, Fore, Back
 from time import time
-import sys, os
-from getpass import getpass
+import sys, os, configparser
 
 init(autoreset=True)
-arquivo = 'sinais.txt'
+arquivo = 'lista.csv'
 
 
 def Conexao():
 	global API
-	print('=========================================\n|   INSIRA E-MAIL E SENHA DA IQOPTION   |\n=========================================')
-	email = str(input('E-mail: '))
-	senha = getpass()
-	Clear_Screen()
+	email = config['email']
+	senha = config['senha']
 	print('Conectando....')
 	API = IQ_Option(email, senha)
 	API.connect()
 
 	if API.check_connect():
 		Clear_Screen()
-		print(' Conectado com sucesso!')
+		print('Conectado com sucesso!')
 	else:
-		print(' Erro ao conectar')
-		input('\n\n Aperte enter para sair')
+		print('Erro ao conectar')
 		sys.exit()
+
+
+def Configuracoes():
+	global config
+	config = {}
+	arquivo = configparser.RawConfigParser()
+	arquivo.read('config.txt')
+
+	config['email'] = arquivo.get('CONTA', 'email')
+	config['senha'] = arquivo.get('CONTA', 'senha')
+	timeframe_str = arquivo.get('CONFIGURACOES', 'timeframe')
+	# Transforma a string timeframe em uma lista com os timeframes
+	config['timeframe'] = list(timeframe_str.split(','))
+	config['dias'] = arquivo.get('CONFIGURACOES', 'dias')
+	config['porcentagem'] = arquivo.get('CONFIGURACOES', 'porcentagem')
+	config['martingale'] = arquivo.get('CONFIGURACOES', 'martingale')
 
 
 def Clear_Screen():
@@ -76,19 +88,18 @@ def cataloga(par, dias, prct_call, prct_put, timeframe):
 	return analise
 
 
+print('=========================================\n|         CATALOGADOR DE SINAIS         |\n=========================================')
+
+Configuracoes()
 Conexao()
 
-print('\n\nQual timeframe deseja analisar?: ', end='')
-timeframe = int(input())
+timeframe_config = config['timeframe']
 
-print('\nQuantos dias deseja analisar?: ', end='')
-dias = int(input())
+dias = int(config['dias'])
 
-print('\nPorcentagem minima?: ', end='')
-porcentagem = int(input())
+porcentagem = int(config['porcentagem'])
 
-print('\nQuantos Martingales?: ', end='')
-martingale = input()
+martingale = config['martingale']
 
 prct_call = abs(porcentagem)
 prct_put = abs(100 - porcentagem)
@@ -96,67 +107,69 @@ prct_put = abs(100 - porcentagem)
 P = API.get_all_open_time()
 
 print('\n\n')
+try:
+	for timeframe in timeframe_config:
+		catalogacao = {}
+		for par in P['digital']:
+			if P['digital'][par]['open'] == True:
+				timer = int(time())
+				print(f'{Fore.GREEN}CATALOGANDO - {Fore.RESET} {Fore.BLUE}{par}{Fore.RESET} | TIMEFRAME {Fore.GREEN}M{timeframe}{Fore.RESET}...', end='')
+				catalogacao.update({par: cataloga(par, dias, prct_call, prct_put, timeframe)})
 
-catalogacao = {}
-for par in P['digital']:
-	if P['digital'][par]['open'] == True:
-		timer = int(time())
-		print(Fore.GREEN + '*' + Fore.RESET + ' CATALOGANDO - ' + par + '.. ', end='')
+				for par in catalogacao:
+					for horario in sorted(catalogacao[par]):
+						if martingale.strip() != '':
 
-		catalogacao.update({par: cataloga(par, dias, prct_call, prct_put, timeframe)})
+							mg_time = horario
+							soma = {'verde': catalogacao[par][horario]['verde'], 'vermelha': catalogacao[par][horario]['vermelha'], 'doji': catalogacao[par][horario]['doji']}
+
+							for i in range(int(martingale)):
+
+								catalogacao[par][horario].update({'mg' + str(i + 1): {'verde': 0, 'vermelha': 0, 'doji': 0, '%': 0}})
+
+								mg_time = str(datetime.strptime((datetime.now()).strftime('%Y-%m-%d ') + str(mg_time), '%Y-%m-%d %H:%M') + timedelta(minutes=timeframe))[11:-3]
+
+								if mg_time in catalogacao[par]:
+									catalogacao[par][horario]['mg' + str(i + 1)]['verde'] += catalogacao[par][mg_time]['verde'] + soma['verde']
+									catalogacao[par][horario]['mg' + str(i + 1)]['vermelha'] += catalogacao[par][mg_time]['vermelha'] + soma['vermelha']
+									catalogacao[par][horario]['mg' + str(i + 1)]['doji'] += catalogacao[par][mg_time]['doji'] + soma['doji']
+
+									catalogacao[par][horario]['mg' + str(i + 1)]['%'] = round(100 * (catalogacao[par][horario]['mg' + str(i + 1)]['verde' if catalogacao[par][horario]['dir'] == 'CALL' else 'vermelha'] / (catalogacao[par][horario]['mg' + str(i + 1)]['verde'] + catalogacao[par][horario]['mg' + str(i + 1)]['vermelha'] + catalogacao[par][horario]['mg' + str(i + 1)]['doji'])))
+
+									soma['verde'] += catalogacao[par][mg_time]['verde']
+									soma['vermelha'] += catalogacao[par][mg_time]['vermelha']
+									soma['doji'] += catalogacao[par][mg_time]['doji']
+								else:
+									catalogacao[par][horario]['mg' + str(i + 1)]['%'] = 0
+
+				print('finalizado em ' + str(int(time()) - timer) + ' segundos')
+
+		print('\n\n')
 
 		for par in catalogacao:
 			for horario in sorted(catalogacao[par]):
-				if martingale.strip() != '':
+				ok = False
 
-					mg_time = horario
-					soma = {'verde': catalogacao[par][horario]['verde'], 'vermelha': catalogacao[par][horario]['vermelha'], 'doji': catalogacao[par][horario]['doji']}
-
-					for i in range(int(martingale)):
-
-						catalogacao[par][horario].update({'mg' + str(i + 1): {'verde': 0, 'vermelha': 0, 'doji': 0, '%': 0}})
-
-						mg_time = str(datetime.strptime((datetime.now()).strftime('%Y-%m-%d ') + str(mg_time), '%Y-%m-%d %H:%M') + timedelta(minutes=timeframe))[11:-3]
-
-						if mg_time in catalogacao[par]:
-							catalogacao[par][horario]['mg' + str(i + 1)]['verde'] += catalogacao[par][mg_time]['verde'] + soma['verde']
-							catalogacao[par][horario]['mg' + str(i + 1)]['vermelha'] += catalogacao[par][mg_time]['vermelha'] + soma['vermelha']
-							catalogacao[par][horario]['mg' + str(i + 1)]['doji'] += catalogacao[par][mg_time]['doji'] + soma['doji']
-
-							catalogacao[par][horario]['mg' + str(i + 1)]['%'] = round(100 * (catalogacao[par][horario]['mg' + str(i + 1)]['verde' if catalogacao[par][horario]['dir'] == 'CALL' else 'vermelha'] / (catalogacao[par][horario]['mg' + str(i + 1)]['verde'] + catalogacao[par][horario]['mg' + str(i + 1)]['vermelha'] + catalogacao[par][horario]['mg' + str(i + 1)]['doji'])))
-
-							soma['verde'] += catalogacao[par][mg_time]['verde']
-							soma['vermelha'] += catalogacao[par][mg_time]['vermelha']
-							soma['doji'] += catalogacao[par][mg_time]['doji']
-						else:
-							catalogacao[par][horario]['mg' + str(i + 1)]['%'] = 0
-
-		print('finalizado em ' + str(int(time()) - timer) + ' segundos')
-
-print('\n\n')
-
-for par in catalogacao:
-	for horario in sorted(catalogacao[par]):
-		ok = False
-
-		if catalogacao[par][horario]['%'] >= porcentagem:
-			ok = True
-		else:
-			for i in range(int(martingale)):
-				if catalogacao[par][horario]['mg' + str(i + 1)]['%'] >= porcentagem:
+				if catalogacao[par][horario]['%'] >= porcentagem:
 					ok = True
-					break
+				else:
+					for i in range(int(martingale)):
+						if catalogacao[par][horario]['mg' + str(i + 1)]['%'] >= porcentagem:
+							ok = True
+							break
 
-		if ok == True:
+				if ok == True:
 
-			msg = Fore.YELLOW + par + Fore.RESET + ' - ' + horario + ' - ' + (Fore.RED if catalogacao[par][horario]['dir'] == 'PUT ' else Fore.GREEN) + catalogacao[par][horario]['dir'] + Fore.RESET + ' - ' + str(catalogacao[par][horario]['%']) + '% - ' + Back.GREEN + Fore.BLACK + str(catalogacao[par][horario]['verde']) + Back.RED + Fore.BLACK + str(catalogacao[par][horario]['vermelha']) + Back.RESET + Fore.RESET + str(catalogacao[par][horario]['doji'])
+					msg = Fore.YELLOW + par + Fore.RESET + ' - ' + horario + ' - ' + (Fore.RED if catalogacao[par][horario]['dir'] == 'PUT ' else Fore.GREEN) + catalogacao[par][horario]['dir'] + Fore.RESET + ' - ' + str(catalogacao[par][horario]['%']) + '% - ' + Back.GREEN + Fore.BLACK + str(catalogacao[par][horario]['verde']) + Back.RED + Fore.BLACK + str(catalogacao[par][horario]['vermelha']) + Back.RESET + Fore.RESET + str(catalogacao[par][horario]['doji'])
 
-			if martingale.strip() != '':
-				for i in range(int(martingale)):
-					if str(catalogacao[par][horario]['mg' + str(i + 1)]['%']) != 'N/A':
-						msg += ' | MG ' + str(i + 1) + ' - ' + str(catalogacao[par][horario]['mg' + str(i + 1)]['%']) + '% - ' + Back.GREEN + Fore.BLACK + str(catalogacao[par][horario]['mg' + str(i + 1)]['verde']) + Back.RED + Fore.BLACK + str(catalogacao[par][horario]['mg' + str(i + 1)]['vermelha']) + Back.RESET + Fore.RESET + str(catalogacao[par][horario]['mg' + str(i + 1)]['doji'])
-					else:
-						msg += ' | MG ' + str(i + 1) + ' - N/A - N/A'
+					if martingale.strip() != '':
+						for i in range(int(martingale)):
+							if str(catalogacao[par][horario]['mg' + str(i + 1)]['%']) != 'N/A':
+								msg += ' | MG ' + str(i + 1) + ' - ' + str(catalogacao[par][horario]['mg' + str(i + 1)]['%']) + '% - ' + Back.GREEN + Fore.BLACK + str(catalogacao[par][horario]['mg' + str(i + 1)]['verde']) + Back.RED + Fore.BLACK + str(catalogacao[par][horario]['mg' + str(i + 1)]['vermelha']) + Back.RESET + Fore.RESET + str(catalogacao[par][horario]['mg' + str(i + 1)]['doji'])
+							else:
+								msg += ' | MG ' + str(i + 1) + ' - N/A - N/A'
 
-			print(msg)
-			open(arquivo, 'a').write('M' + str(timeframe) + ';' + par + ';' + horario + ';' + catalogacao[par][horario]['dir'].strip() + '\n')
+					print(msg)
+					open(arquivo, 'a').write('M' + str(timeframe) + ';' + par + ';' + horario + ';' + catalogacao[par][horario]['dir'].strip() + '\n')
+except KeyboardInterrupt:
+	sys.exit()
